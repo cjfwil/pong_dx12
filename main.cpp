43b28d894910e6c114b9829b2f941de7dcb00d4d
cjfwil <23485052+cjfwil@users.noreply.github.com>
2026-02-03 04:00:16 +0000

    sdl window and loop

--- main.cpp
+++ main.cpp
@@ -2,31 +2,55 @@
 
 #include <SDL3/SDL.h>
 
-static void log_error(const char* msg="Unknown error")
+static void log_error(const char *msg = "Unknown error")
 {
     SDL_Log("[ERROR]: %s", msg);
 }
 
-static void log_sdl_error(const char* msg="Unknown SDL error")
+static void log_sdl_error(const char *msg = "Unknown SDL error")
 {
     SDL_Log("[SDL ERROR]: %s: %s", msg, SDL_GetError());
 }
 
 int main(void)
 {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
         log_sdl_error("Couldn't initialise SDL");
         return 1;
-    } else {
+    }
+    else
+    {
         SDL_Log("SDL Video initialised.");
     }
 
-    SDL_Window* window = SDL_CreateWindow("Name", 640, 480, SDL_WINDOW_RESIZABLE);
-    if (window == nullptr) {
+    SDL_Window *window = SDL_CreateWindow("Name", 640, 480, SDL_WINDOW_RESIZABLE);
+    if (window == nullptr)
+    {
         log_sdl_error("Couldn't create SDL window");
         return 1;
-    } else {
+    }
+    else
+    {
         SDL_Log("SDL Window created.");
     }
-    return(0);
+
+    static bool isRunning = true;
+    while (isRunning)
+    {
+        SDL_Event sdlEvent;
+        while (SDL_PollEvent(&sdlEvent))
+        {
+            switch (sdlEvent.type)
+            {
+            case SDL_EVENT_QUIT:
+            {
+                isRunning = false;
+            }
+            break;
+            }
+        }
+    }
+
+    return (0);
 }
